2a2fd0b6dbfd1010a4930bbe0297a42a5ded4b5d
khomsark <komsark_@hotmail.co.th>
2020-12-10 22:11:48 +0700

    commit

---
+++ addons/computer_store/models/component.py
@@ -0,0 +1,40 @@
+
+from odoo import models, fields, api
+
+class component(models.Model):
+    _name = 'component'
+    _description = 'component for computer'
+
+name = fields.Char( #ชื่อของชิ้นส่วน
+    string='Component name', 
+    required=True,
+    )
+brand = fields.Char(  #ยี่ห้อชิ้นส่วน
+    string='Brand of product',
+    required=True,
+    )
+count = fields.Integer(  #จำนวนสินค้า
+    string='Count',
+    required=True,
+    )
+vendor = fields.Char(  #บริษัทที่รับมา
+    string='Vendor',
+    required=True,
+    )
+saleprice = fields.Float(  #ราคาขาย
+    string='Saleprice',
+    required=True,
+       )
+cost = fields.Float( #ต้นทุนที่รับมา
+    string='Cost',
+    required=True,
+       )
+orderdate = fields.Char(  #วันที่สั่งซื้อ
+    string='Orderdate',
+    required=True,
+    )       
+product_id = fields.Many2one(  #product ที่ใช้กับชิ้นส่วน
+        comodel_name='product',
+        string='product',
+    )
+    
---
+++ addons/computer_store/models/product.py
@@ -0,0 +1,41 @@
+ 
+ 
+from odoo import models, fields, api
+
+class product(models.Model):
+    _name = 'product'
+    _description = 'product description'
+
+   name = fields.Char( #ชื่อสินค้า
+    string='Product Name', 
+    required=True,
+    )
+   picture = fields.Char() #รูปภ่าพ
+   brand = fields.Char(  #ยี่ห้อของสินค้า
+    string='brand Product',
+    required=True,
+    )
+    count = fields.Integer(  #จำนวนสินค้า
+    string='Count',
+    required=True,
+    )
+   vendor = fields.Char(  #บริษัทที่รับมา
+    string='Vendor',
+    required=True,
+    )
+   saleprice = fields.Float(  #ราคาขาย
+    string='Saleprice',
+    required=True,
+       )
+    cost = fields.Float( #ต้นทุนที่รับมา
+    string='Cost',
+    required=True,
+       )
+component_id = fields.One2many(  #ชิ้นส่วนของ product
+        comodel_name='component',
+        string='component',
+    )
+orderdate = fields.Char(  #วันที่สั่งซื้อ
+    string='Orderdate',
+    required=True,
+    )
